df3da8dbfa2fd6be332c32360c8c7bf13bf0e5d7
fpiskur <filip.piskur@gmail.com>
2023-07-16 13:18:01 +0200

    Dynamically show sub-categories

--- app/javascript/controllers/load_sub_categories_controller.js
+++ app/javascript/controllers/load_sub_categories_controller.js
@@ -3,6 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="load-sub-categories"
 export default class extends Controller {
   load(event) {
-    
+    let parentId = event.target.value;
+    let subCatGroups = document.querySelectorAll('.sub-cat-group');
+    let targetElement = document.getElementById(`sub-cat-${parentId}`);
+
+    if (subCatGroups) {
+      subCatGroups.forEach(group => {
+        group.classList.add('hidden');
+      });
+    }
+
+    targetElement.classList.remove('hidden');
   }
 }
