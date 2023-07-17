9a4a88f25643f3bb9846129f2a5898ac48ea9539
fpiskur <filip.piskur@gmail.com>
2023-07-17 21:27:35 +0200

    Fix category validation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,5 +2,14 @@ class Expense < ApplicationRecord
   belongs_to :category
   accepts_nested_attributes_for :category
 
+  validate :parent_category_with_children_selected
   validates_presence_of :amount, :date, :description, :category_id
+
+  private
+
+  def parent_category_with_children_selected
+    if self.category&.sub_categories&.any?
+      errors.add(:category, 'has sub-categories. Please pick one.')
+    end
+  end
 end
