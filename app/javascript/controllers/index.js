733fb92afc787442fb79d8df55782802807769bd
fpiskur <filip.piskur@gmail.com>
2023-07-18 20:48:11 +0200

    Expenses form styling, add areas to nav

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import LoadSubCategoriesController from "./load_sub_categories_controller"
 application.register("load-sub-categories", LoadSubCategoriesController)
--- app/javascript/controllers/load_sub_categories_controller.js
+++ app/javascript/controllers/load_sub_categories_controller.js
@@ -5,8 +5,10 @@ export default class extends Controller {
   load(event) {
     let parentId = event.target.value;
     let subCatGroups = document.querySelectorAll('.sub-cat-group');
+    let categoryLabels = document.querySelectorAll('.custom-radio-btn');
     let targetElement = document.getElementById(`sub-cat-${parentId}`);
     let parentCategoryBtn = document.getElementById(`expense_category_id_${parentId}`);
+    let parentCategoryLabel = document.querySelector(`label[for=expense_category_id_${parentId}]`);
 
     if (subCatGroups) {
       subCatGroups.forEach(group => {
@@ -14,8 +16,17 @@ export default class extends Controller {
       });
     }
 
+    if (categoryLabels) {
+      categoryLabels.forEach(label => {
+        label.classList.remove('btn--light');
+        label.classList.add('btn--primary');
+      })
+    }
+
     if (targetElement) {
       parentCategoryBtn.checked = false;
+      parentCategoryLabel.classList.remove('btn--primary');
+      parentCategoryLabel.classList.add('btn--light');
       targetElement.classList.remove('hidden');
     }
   }
