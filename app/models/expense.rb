8fa1ceef16115033ca22fcf486d1a878530bdbd7
fpiskur <filip.piskur@gmail.com>
2023-07-27 11:47:36 +0200

    Return parent category validation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,5 +6,14 @@ class Expense < ApplicationRecord
   has_many :areas, through: :expenses_areas
   accepts_nested_attributes_for :areas
 
+  validate :parent_category_with_children_selected
   validates_presence_of :amount, :date, :description, :category_id
+
+  private
+
+  def parent_category_with_children_selected
+    if self.category&.sub_categories&.any?
+      errors.add(:category, 'has sub-categories. Please pick one.')
+    end
+  end
 end
