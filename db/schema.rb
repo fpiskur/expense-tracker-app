0cbdd9430cd7f3462d3902601a6f0a3abcac9d8e
fpiskur <filip.piskur@gmail.com>
2024-06-21 21:45:53 +0200

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,57 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_140358) do
+ActiveRecord::Schema[7.0].define(version: 20_230_728_140_358) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "areas", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'areas', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "parent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["parent_id"], name: "index_categories_on_parent_id"
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'parent_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['parent_id'], name: 'index_categories_on_parent_id'
   end
 
-  create_table "expenses", force: :cascade do |t|
-    t.decimal "amount", precision: 9, scale: 2
-    t.string "description"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_expenses_on_category_id"
+  create_table 'expenses', force: :cascade do |t|
+    t.decimal 'amount', precision: 9, scale: 2
+    t.string 'description'
+    t.date 'date'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'category_id'
+    t.index ['category_id'], name: 'index_expenses_on_category_id'
   end
 
-  create_table "expenses_areas", force: :cascade do |t|
-    t.bigint "expense_id", null: false
-    t.bigint "area_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["area_id"], name: "index_expenses_areas_on_area_id"
-    t.index ["expense_id"], name: "index_expenses_areas_on_expense_id"
+  create_table 'expenses_areas', force: :cascade do |t|
+    t.bigint 'expense_id', null: false
+    t.bigint 'area_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['area_id'], name: 'index_expenses_areas_on_area_id'
+    t.index ['expense_id'], name: 'index_expenses_areas_on_expense_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "categories", "categories", column: "parent_id"
-  add_foreign_key "expenses", "categories"
-  add_foreign_key "expenses_areas", "areas"
-  add_foreign_key "expenses_areas", "expenses"
+  add_foreign_key 'categories', 'categories', column: 'parent_id'
+  add_foreign_key 'expenses', 'categories'
+  add_foreign_key 'expenses_areas', 'areas'
+  add_foreign_key 'expenses_areas', 'expenses'
 end
