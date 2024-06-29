cf9a65f0aeee282bbe5636cb06d08d1b25823d0d
fpiskur <filip.piskur@gmail.com>
2024-06-29 12:40:00 +0200

    Add authentication spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentication', type: :request do
-  it 'should redirect to login page'
+  let(:user) { create(:user) }
+  it 'should redirect to login page' do
+    get root_path
+    expect(response).to redirect_to new_user_session_path
+  end
+
+  it 'should respond with the requested page for logged in user' do
+    sign_in user
+    get root_path
+    expect(response.status).to eq(200)
+  end
 end
